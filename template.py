b3304eb4cbe0cd3afd8b6a35e27a1b6b7f463185
Théo <theodore.cousin@student.uclouvain.be>
2022-04-04 14:09:56 +0200

    template

---
+++ template.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def pageRankLinear(A, alpha, v):
+    """
+    Input : Une matrice d’adjacence 1 A d’un graphe dirigé,
+    pondéré et régulier G, un vecteur de personnalisation v,
+    ainsi qu’un paramètre de téléportation α compris entre
+    0 et 1 (0.9 par défaut et pour les résultats à présenter).
+    Toutes ces valeurs sont nonnégatives.
+
+    – Output : Un vecteur x contenant les scores
+    d’importance des noeuds ordonnés dans
+    le même ordre que la matrice d’adjacence.
+    """
+
+    return
+
+
+def pageRankPower(A, alpha, v):
+    """
+    – Input : Une matrice d’adjacence A d’un graphe dirigé,
+    pondéré et régulier G, un vecteur de personnalisation v,
+    ainsi qu’un paramètre de téléportation α compris entre 0
+    et 1, α ∈]0,1[ (0.9 par défaut et pour les résultats à présenter).
+
+    – Output : Un vecteur x contenant les scores
+    d’importance des noeuds ordonnés dans
+    le même ordre que la matrice d’adjacence.
+    """
+
+    return
+
+
+
+
