5c46a191f164712531bd86d75dbdf500c853622f
Théo <theodore.cousin@student.uclouvain.be>
2022-04-06 11:48:53 +0200

    PageRankLinear working without alpha & v

--- template.py
+++ template.py
@@ -1,8 +1,9 @@
+import numpy
 import numpy as np
 
 def pageRankLinear(A, alpha, v):
     """
-    Input : Une matrice d’adjacence 1 A d’un graphe dirigé,
+    Input : Une matrice d’adjacence A d’un graphe dirigé,
     pondéré et régulier G, un vecteur de personnalisation v,
     ainsi qu’un paramètre de téléportation α compris entre
     0 et 1 (0.9 par défaut et pour les résultats à présenter).
@@ -13,8 +14,25 @@ def pageRankLinear(A, alpha, v):
     le même ordre que la matrice d’adjacence.
     """
 
-    return
+    n = len(A)
+    I = np.identity(n)
+    dout = np.ones(n)
+    din = np.ones(n)
+    P = A
 
+    for i in range(n):
+        dout[i] = np.sum(A[i])
+        din[i] = np.sum(A[:, i])
+        P[i] = A[i] / dout[i]
+
+    Pfinal = numpy.delete(
+        np.append([np.ones(n)], I-np.transpose(P), axis=0),
+        n, 0)
+
+    vect = np.ones(n)
+    vect[1:n] = 0
+
+    return np.linalg.solve(Pfinal, vect)
 
 def pageRankPower(A, alpha, v):
     """
@@ -32,4 +50,8 @@ def pageRankPower(A, alpha, v):
 
 
 
-
+print(pageRankLinear(np.array([
+    [0.0, 2.0, 0.0],
+    [0.0, 0.0, 2.0],
+    [1.0, 1.0, 0.0]
+]), 1.0, 1.0))
