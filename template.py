0fefb6e1da249f8bfb6274b0a68b205905efcfcd
Théo <theodore.cousin@student.uclouvain.be>
2022-04-06 12:23:55 +0200

    PageRankPower working without alpha & v

--- template.py
+++ template.py
@@ -34,6 +34,13 @@ def pageRankLinear(A, alpha, v):
 
     return np.linalg.solve(Pfinal, vect)
 
+def error(a, b):
+    Max = 0
+    if (len(a) != len(b)): raise IndexError('a and b must be equally sized')
+    for i in range(len(a)):
+        Max = max(abs(a[i] - b[i]), Max)
+    return Max
+
 def pageRankPower(A, alpha, v):
     """
     – Input : Une matrice d’adjacence A d’un graphe dirigé,
@@ -46,11 +53,34 @@ def pageRankPower(A, alpha, v):
     le même ordre que la matrice d’adjacence.
     """
 
-    return
+    n = len(A)
+    I = np.identity(n)
+    dout = np.ones(n)
+    din = np.ones(n)
+    P = A
+    eps = 10**-8
+
+    for i in range(n):
+        dout[i] = np.sum(A[i])
+        din[i] = np.sum(A[:, i])
+        P[i] = A[i] / dout[i]
+
+    Pt = np.transpose(P)
+    dprev = np.ones(n)*np.inf
+
+    it = 0
+
+    while error(dprev, din) > eps:
+        dprev = din
+        din = np.dot(Pt, din)
+        it += 1
+
+    print(it)
+    return din/np.sum(din)
 
 
 
-print(pageRankLinear(np.array([
+print(pageRankPower(np.array([
     [0.0, 2.0, 0.0],
     [0.0, 0.0, 2.0],
     [1.0, 1.0, 0.0]
